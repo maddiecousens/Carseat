f64eb33665f2a9a2dbfd2c522b8f59a6ff4a4500
Maddie Cousens <mscousens@gmail.com>
2016-10-31 21:27:49 +0000

    Added preliminary seed data

--- model.py
+++ model.py
@@ -7,6 +7,98 @@ db = SQLAlchemy()
 ##############################################################################
 # Database Models
 
+class User(db.Model):
+    """User of the site. Can be driver (as of now)"""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(64), nullable=True)
+    password = db.Column(db.String(64), nullable=True)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<User user_id=%s, email=%s, password=%s>" % (self.user_id, 
+                                            self.email, self.password)
+
+class Ride(db.Model):
+    """A specific ride"""
+
+    __tablename__ = "rides"
+
+    ride_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    driver = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    start_location = db.Column(db.String(100), nullable=False)
+    end_location = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    seats = db.Column(db.Integer, nullable=False)
+
+    user = db.relationship("User",
+                            backref=db.backref("rides_offered"))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Ride ride_id={}, driver={}, start={}, end={}, date={}>".format(self.ride_id, 
+            self.driver, self.start_location, self.end_location, self.date)
+
+
+class Rider(db.Model):
+    """Association table. Users taking Rides"""
+
+    __tablename__ = "riders"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    ride_id = db.Column(db.Integer, db.ForeignKey('rides.ride_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    seats = db.Column(db.Integer, nullable=False)
+
+    user = db.relationship("User",
+                            backref=db.backref("rides_taking"))
+
+    ride = db.relationship("Ride",
+                            backref=db.backref("riders"))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Rider id={}, ride_id={}, user_id={}>".format(self.id, 
+            self.ride_id, self.user_id)
+
+# class Request(db.Model):
+#     """Pending Requests"""
+
+#     id = db.Column(db.Integer, nullable=False, autoincrement=True, primary_key=True)
+#     ride_id = db.Column(db.Integer, nullable=False, db.ForeignKey('riders.ride_id'))
+#     user_id = db.Column(db.Integer, nullable=False, db.ForeignKey('users.user_id'))
+#     seats = db.Column(db.Integer, nullable=False)
+
+#  id | ride_id | user_id | seats 
+# ----+---------+---------+-------
+#   1 |       1 |       3 |     2
+#   2 |       1 |       4 |     2
+#   3 |       2 |       1 |     1
+#   4 |       2 |       4 |     1
+# (4 rows)
+
+# rideshare=# select * from users;
+#  user_id |  name  |  email  | password 
+# ---------+--------+---------+----------
+#        1 | Maddie | maddie@ | doge1
+#        2 | Ahmad  | ahmad@  | doge2
+#        3 | Carl   | carl@   | doge3
+#        4 | Lexie  | lexie@  | doge4
+# (4 rows)
+
+# rideshare=# select * from rides;
+#  ride_id | driver | start_location | end_location |            date            | seats 
+# ---------+--------+----------------+--------------+----------------------------+-------
+#        1 |      1 | SF             | Tahoe        | 2016-10-31 21:17:37.659467 |     4
+#        2 |      2 | SF             | LA           | 2016-10-31 21:17:37.659565 |     4
+
+
 
 
 
@@ -28,5 +120,5 @@ def connect_db(app):
 if __name__ == "__main__":
 
     from server import app
-    connect_to_db(app)
+    connect_db(app)
     print "Connected to Database"
---
+++ seed.py
@@ -0,0 +1,44 @@
+from model import User, Ride, Rider
+
+from model import connect_db, db
+from server import app
+
+from datetime import datetime
+
+def example_data():
+    """Create some sample data."""
+
+    # In case this is run more than once, empty out existing data
+    User.query.delete()
+    Ride.query.delete()
+    Rider.query.delete()
+
+   # Add sample employees and departments
+    maddie = User(user_id=1 ,name="Maddie", email="maddie@", password="doge1")
+    ahmad = User(user_id=2 ,name="Ahmad", email="ahmad@", password="doge2")
+    carl = User(user_id=3 ,name="Carl", email="carl@", password="doge3")
+    lexie = User(user_id=4 ,name="Lexie", email="lexie@", password="doge4")
+
+    # = User(name="", email="", password="")
+
+    sfth =Ride(ride_id=1 ,driver=1, start_location="SF", end_location="Tahoe",date=datetime.now(), seats=4)
+    sfla =Ride(ride_id=2 ,driver=2, start_location="SF", end_location="LA",date=datetime.now(), seats=4)
+
+    # Ride(driver=, start_location="", end_location="",date =)
+
+    rider1 = Rider(id=1, ride_id=1, user_id=3, seats=2)
+    rider2 = Rider(id=2, ride_id=1, user_id=4, seats=2)
+    rider3 = Rider(id=3, ride_id=2, user_id=1, seats=1)
+    rider4 = Rider(id=4, ride_id=2, user_id=4, seats=1)
+
+    db.session.add_all([maddie, ahmad, carl, lexie, sfth, sfla, rider1, rider2, rider3, rider4])
+    db.session.commit()
+
+
+
+if __name__ == "__main__":
+    connect_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+    example_data()
